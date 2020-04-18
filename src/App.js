96c216ba7eb030f100f2ffdfd9e0f42b9fd562d1
Andrew Doan <andrewdoancs@gmail.com>
2020-04-18 00:19:10 -0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route} from "react-router-dom";
 import './App.css';
+import NewsFeed from "./pages/newsFeed"
+import Marketplace from "./pages/marketplace"
+import Account from "./pages/account"
+import Blog from "./pages/blog"
+import Calendar from "./pages/calendar"
 
 function App() {
   return (
     <div>
-      Deadstock
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={NewsFeed} />
+          <Route exact path="/marketplace" component={Marketplace} />
+          <Route exact path="/account" component={Account} />
+          <Route exact path="/blog" component={Blog} />
+          <Route exact path="/calendar" component={Calendar} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
