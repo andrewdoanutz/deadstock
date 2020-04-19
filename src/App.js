c97f78ede4a35071960018ccd8cecf29e1c4073b
Andrew Doan <andrewdoancs@gmail.com>
2020-04-19 01:45:39 -0700

    account pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import NewsFeed from "./pages/newsFeed"
 import Marketplace from "./pages/marketplace"
 import Account from "./pages/account"
+import MakeAccount from "./pages/makeAccount"
 import Blog from "./pages/blog"
 import Calendar from "./pages/calendar"
 import NavBar from "./components/navbar"
@@ -18,6 +19,7 @@ function App() {
             <Route exact path="/" component={NewsFeed} />
             <Route exact path="/marketplace" component={Marketplace} />
             <Route exact path="/account" component={Account} />
+            <Route exact path="/createAccount" component={MakeAccount} />
             <Route exact path="/blog" component={Blog} />
             <Route exact path="/calendar" component={Calendar} />
           </div>
--- src/pages/makeAccount.js
+++ src/pages/makeAccount.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import {Card,Button,Form,Col,Row} from "react-bootstrap"
 
-export default class Account extends Component {
+export default class MakeAccount extends Component {
   render() {
     return (
       <>
@@ -10,12 +10,20 @@ export default class Account extends Component {
           <Col sm={6} style={{marginTop:"5%"}}>
             
             <Card style={{backgroundColor:"#212121",color:"white"}}>
-              <Card.Title style={{margin:"4%",marginBottom:"0",fontWeight:"bold",letterSpacing:".05em",fontSize:"1.5em",textAlign:"center"}}>Log In</Card.Title>
+              <Card.Title style={{margin:"4%",marginBottom:"0",fontWeight:"bold",letterSpacing:".05em",fontSize:"1.5em",textAlign:"center"}}>Create Account</Card.Title>
               <Card.Body style={{marginLeft:"2%",marginRight:"2%"}}>
                     <Form >
                       <Form.Group controlId="formBasicEmail">
                         <Form.Label>Username</Form.Label>
                         <Form.Control type="username" placeholder="Enter username" />
+                      </Form.Group>
+                      <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="password" placeholder="Enter email" />
+                      </Form.Group>
+                      <Form.Group controlId="formBasicPhone">
+                        <Form.Label>Phone Number</Form.Label>
+                        <Form.Control type="phone" placeholder="Enter phone number" />
                       </Form.Group>
                       <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
@@ -30,8 +38,8 @@ export default class Account extends Component {
               </Card.Body>
             </Card>
             <div style={{textAlign:"center", marginTop:"10%"}}>
-              <a className="accountSignup" href="\createAccount">
-                Create Account
+              <a className="accountSignup" href="\account">
+                Log In
               </a>
             </div>
           </Col>
